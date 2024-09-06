351371981169fde12860483c0a25ece3ab212809
naumanrafique12 <naumanrafique10.com>
2024-09-07 02:16:55 +0500

    First CI

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-def add(a,b):
-    return a+b 
+def add(a, b):
+    return a + b
 
-def sub(a,b):
-    return a-b 
 
-print(add(2,3))
-print(sub(2,3))
+def sub(a, b):
+    return a - b
+
+
+print(add(2, 3))
+print(sub(2, 3))
